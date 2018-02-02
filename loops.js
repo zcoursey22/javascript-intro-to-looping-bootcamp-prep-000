488728d4f19c093eb07963f7ece43bf7b2a01f43
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 00:28:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,6 +4,10 @@ function forLoop(array) {
   }
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function whileLoop(array) {
   while (maybeTrue() && array.length > 0) {
     
