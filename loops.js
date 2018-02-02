601671d233896ed4475f0d18e7981ffdf80a8e01
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 00:24:17 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  }
+}
+
+function whileLoop(array) {
+  counter = 0
+  while (counter < array.length) {
+    
+    counter++;
+  }
+}
