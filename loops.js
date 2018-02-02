4be8ee879f63ee14e39000407f5371cc2901f3ed
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 00:41:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,7 @@ function whileLoop(n) {
 function maybeTrue() {
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
+
+function doWhileLoop(array) {
+  
+}
